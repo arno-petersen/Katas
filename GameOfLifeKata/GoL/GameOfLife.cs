80d486b939852883530e1668944aba384d971167
arno <arno@ERDE>
2020-07-18 16:20:42 +0200

    Removed unused usings

--- GameOfLifeKata/GoL/GameOfLife.cs
+++ GameOfLifeKata/GoL/GameOfLife.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 [assembly: InternalsVisibleTo("GameOfLifeTests")]
 namespace GoL
